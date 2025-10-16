e8fdcbe47831dd858de3c2fe6e595b3e2f64fa06
Danish Furekh Dar <68053957+danishfurekhdar@users.noreply.github.com>
2025-10-16 12:19:43 +0200

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -16,9 +16,10 @@ REM cls  REM Clear the screen
 REM echo Enter username and password to upload files to Website
 REM set /p username=Enter your username: 
 REM scp -r _site/* %username%@webserv3.rz.uni-jena.de:web/
-scp -r _site/* Atthrandolf@webserv3.rz.uni-jena.de:~/web/
+scp -4 -r _site/* atthrandolf@webserv3.rz.uni-jena.de:web/
 echo All commands executed.
 
 pause
+
 
 
